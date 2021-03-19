fcfc77daf1c7e03c719f12557569af549823cf04
Nabeel k <nabeel.kottol@gmail.com>
2021-03-20 01:53:16 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.41b0aafa0588810ce8d73dd29fd4d31e.js"
+  "/portfolio/precache-manifest.a46f6c75f84a3c71c07bccd461670d99.js"
 );
 
 self.addEventListener('message', (event) => {

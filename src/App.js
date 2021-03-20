f6c9ea80322270fd655eacba0f3c233684d1be3a
Nabeel k <nabeel.kottol@gmail.com>
2021-03-20 17:59:37 +0530

    format code in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-import * as Icon from 'react-feather';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, {useState} from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import "./App.scss";
 import About from "./pages/About";
 import BlogDetails from "./pages/BlogDetails";
@@ -12,36 +11,26 @@ import Portfolios from "./pages/Portfolios";
 import Resumes from "./pages/Resumes";
 
 function App() {
-  const [lightMode, setLightMode] = useState(false); // Made it true if you want to load your site light mode primary
+    const [lightMode, setLightMode] = useState(false); // Made it true if you want to load your site light mode primary
 
-  lightMode ? document.body.classList.add('light') : document.body.classList.remove('light');
+    lightMode ? document.body.classList.add('light') : document.body.classList.remove('light');
 
-  const handleMode = () => {
-    if (!lightMode) {
-      setLightMode(true);
-      document.body.classList.add('light')
-    } else {
-      setLightMode(false);
-      document.body.classList.remove('light')
-    }
-  }
-
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/portfolio" exact>
-          <Home lightMode={lightMode}/>
-        </Route>
-        <Route path="/about" component={About} />
-        <Route path="/resume" component={Resumes} />
-        <Route path="/portfolios" component={Portfolios} />
-        <Route path="/blogs" exact component={Blogs} />
-        <Route path="/blogs/blog-details/:id/:title" component={BlogDetails} />
-        <Route path="/contact" component={Contact} />
-        <Route path="*" component={Notfound} />
-      </Switch>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/portfolio" exact>
+                    <Home lightMode={lightMode}/>
+                </Route>
+                <Route path="/about" component={About}/>
+                <Route path="/resume" component={Resumes}/>
+                <Route path="/portfolios" component={Portfolios}/>
+                <Route path="/blogs" exact component={Blogs}/>
+                <Route path="/blogs/blog-details/:id/:title" component={BlogDetails}/>
+                <Route path="/contact" component={Contact}/>
+                <Route path="*" component={Notfound}/>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
